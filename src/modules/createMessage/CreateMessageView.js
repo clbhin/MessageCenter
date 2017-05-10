e19858534c291e33b5d802ad3e977a94a55cbc6e
linbing.chen <linbing.chen>
2017-05-10 14:27:37 +0800

    add send message function

--- src/modules/createMessage/CreateMessageView.js
+++ src/modules/createMessage/CreateMessageView.js
@@ -19,31 +19,17 @@ class CreateMessageView extends Component {
     this.state = {
       background: 'red',
       currentMessage:this.props.navigation.state.params,
+      Bcc:this.props.navigation.state.params.Message.Bcc[0],
+      Cc:this.props.navigation.state.params.Message.Cc[0],
+      From:this.props.navigation.state.params.Message.From,
+      Subject:this.props.navigation.state.params.Message.Subject,
+      MessageBody:''
     };
     this.send=this.send.bind(this)
   }
   static displayName = 'MessageDetailView';
 
   static navigationOptions = {
-    /*title: 'CreateMessageView',
-    tabBar: () => ({
-      icon: (props) => (
-        <Icon name='color-lens' size={24} color={props.tintColor} />
-      )
-    }),
-    // TODO: move this into global config?
-    header: {
-      tintColor: 'white',
-      style: {
-        backgroundColor: '#39babd',
-        paddingRight:10
-      },
-    right : (
-      <TouchableOpacity onPress={this.send}>
-        <Icon name={'send'} size={24} color='blue'/>
-      </TouchableOpacity>
-    )
-  }*/
     header:{
       style:{
         height:0
@@ -57,9 +43,15 @@ class CreateMessageView extends Component {
 
   send(){
     let formData=new FormData();
+    formData.append('Bcc',this.state.Bcc);
+    formData.append('Cc',this.state.Cc);
+    formData.append('To',this.state.From);
+    formData.append('Subject',this.state.Subject);
+    this.props.CreateMessageStateActions.sendMessage(formData);
   }
 
   render() {
+    console.log(this)
     return (
       <View>
         <View style={{flexDirection:'row',height:40,borderBottomWidth:1,borderBottomColor:'#ccc',alignItems:'center',backgroundColor: '#39babd'}}>
@@ -67,20 +59,20 @@ class CreateMessageView extends Component {
             <Icon name='arrow-left' size={30} color={'orange'} />
           </TouchableOpacity>
           <Text style={{flex:5,textAlign:'center'}}>reply</Text>
-          <TouchableOpacity  style={{flex:1}}>
+          <TouchableOpacity  style={{flex:1}} onPress={()=>this.send}>
             <Icon name='direction' size={30} color={'orange'} />
           </TouchableOpacity>   
         </View>
         <View style={{marginLeft:10,marginTop:10,marginRight:10,flexDirection:'column'}}>
           <View style={{flexDirection:'row',alignItems:'center',borderBottomWidth:1,borderBottomColor:'#ddd'}}>
             <Text style={{fontSize:16,textAlign: 'center'}}>To:</Text>
-            <TextInput value={this.state.currentMessage.Message.From.PersonName} onChangeText={(PersonName) => {
-              this.setState({'currentMessage.Message.From.PersonName':PersonName})
+            <TextInput value={this.state.From.PersonName} onChangeText={(PersonName) => {
+              this.setState({'From':{'PersonName':PersonName,'Id':PersonName}})
               }} style={{flex:1}}></TextInput>
           </View>
           <View style={{flexDirection:'row',alignItems:'center',borderBottomWidth:1,borderBottomColor:'#ddd'}}>
              <Text style={{fontSize:16,textAlign: 'center'}}>Cc:</Text>
-            <TextInput value={''} style={{flex:1}}></TextInput>
+            <TextInput value={this.state.Cc && this.state.Cc.PersonName} onChange={(text)=>{this.setState({'Cc':{'PersonName':text,'Id':text}})}} style={{flex:1}}></TextInput>
           </View>
           <View style={{flexDirection:'row',alignItems:'center',borderBottomWidth:1,borderBottomColor:'#ddd'}}>
              <Text style={{fontSize:16,textAlign: 'center'}}>Bcc:</Text>
@@ -88,12 +80,12 @@ class CreateMessageView extends Component {
           </View>
           <View style={{flexDirection:'row',alignItems:'center',borderBottomWidth:1,borderBottomColor:'#ddd'}}>
             <Text style={{fontSize:16,textAlign: 'center'}}>Subject:</Text>
-            <TextInput value={'reply:'+this.state.currentMessage.Message.Subject} style={{flex:1}}></TextInput>
+            <TextInput value={this.state.Subject} onChangeText={(Subject)=>{this.setState({Subject})}} style={{flex:1}}></TextInput>
           </View>
           <TextInput
             style={{borderColor: 'gray', minHeight:300,borderWidth: 1}}
-            onChangeText={(text) => this.setState({text})}
-            value={this.state.text}
+            onChangeText={(text) => this.setState({'MessageBody':text})}
+            value={this.state.MessageBody}
             multiline={true}
           />
         </View>
